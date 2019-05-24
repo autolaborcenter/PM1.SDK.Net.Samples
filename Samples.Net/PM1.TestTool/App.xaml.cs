346fd5fa26581cf1b597c07f932fa10598d0d9f3
YdrMaster <ydrml@hotmail.com>
2019-05-24 16:03:54 +0800

    add testtool

---
+++ Samples.Net/PM1.TestTool/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.IO.IsolatedStorage;
+using System.Windows;
+
+namespace Autolabor.PM1.TestTool {
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application { }
+}
