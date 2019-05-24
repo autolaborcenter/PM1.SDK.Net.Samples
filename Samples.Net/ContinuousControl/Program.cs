46ac581a461adb3dc176b6e4f75faac672ca5385
YdrMaster <ydrml@hotmail.com>
2019-05-24 15:44:37 +0800

    change to .net core/nuget still one project cannot run on linux

---
+++ Samples.Net/ContinuousControl/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Autolabor.PM1;
+
+namespace ContinuousControl.Net {
+    class Program {
+        static void Main() {
+            try {
+                Console.WriteLine("initializing...");
+                Console.WriteLine("connected: {0}", Methods.Initialize("", out _));
+                Methods.State = StateEnum.Unlocked;
+
+                Console.WriteLine("--------------");
+                Console.WriteLine("↖↑↗ | q w e");
+                Console.WriteLine("←  → | a   d");
+                Console.WriteLine("↙↓↘ | z x c");
+                Console.WriteLine("--------------");
+                Console.WriteLine("alt + s to exit");
+                Console.WriteLine();
+
+                var running = true;
+                while (running) {
+                    var info = Console.ReadKey(true);
+                    switch (char.ToLower(info.KeyChar)) {
+                        case 'q': // 左前
+                            Methods.VelocityTarget = (+0.1, +0.2);
+                            break;
+                        case 'w': // 前
+                            Methods.VelocityTarget = (+0.1, 0);
+                            break;
+                        case 'e': // 右前
+                            Methods.VelocityTarget = (+0.1, -0.2);
+                            break;
+                        case 'a': // 左
+                            Methods.VelocityTarget = (0, +0.2);
+                            break;
+                        case 's': // 终止
+                            running = info.Modifiers != ConsoleModifiers.Alt;
+                            break;
+                        case 'd': // 右
+                            Methods.VelocityTarget = (0, -0.2);
+                            break;
+                        case 'z': // 左后
+                            Methods.VelocityTarget = (-0.1, -0.2);
+                            break;
+                        case 'x': // 后
+                            Methods.VelocityTarget = (-0.1, 0);
+                            break;
+                        case 'c': // 右后
+                            Methods.VelocityTarget = (-0.1, +0.2);
+                            break;
+                        default: break;
+                    }
+                }
+
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            } finally {
+                Methods.ShutdownSafety();
+            }
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Samples.Net/Startup/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Autolabor.PM1;
+
+namespace Initialization.Net {
+    public static class Program {
+        public static void Main() {
+            try {
+                Console.Write("initialize:\t");
+                // 连接串口
+                Console.WriteLine("{0} connected", Methods.Initialize("", out _));
+                // 解锁
+                Methods.State = StateEnum.Unlocked;            
+                // 以 0.25rad/s 的速度原地转 90°
+                Console.Write("turn around:\t");
+                Methods.TurnAround(0.25, 1.57, out _);   
+                Console.WriteLine("done");
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            } finally {
+                // 断开连接
+                Methods.ShutdownSafety();                      
+            }
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
